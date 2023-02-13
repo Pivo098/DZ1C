e3221cbd444b863c504f198cd2e39d58b08e4709
Ivan Pivnev <pivnev-1998@mail.ru>
2023-02-14 02:14:24 +0300

    написал четвертый пример

---
+++ Primer_4/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число > ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= num; i += 2) // += как я узнал это сочетание символов присваиваение и сложение (i = i + 2)
+    Console.WriteLine(i);
